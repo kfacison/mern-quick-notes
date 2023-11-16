61727c6edaf8761eef49e044a06849ec966d8629
kfac <kfacison1998@gmail.com>
2023-11-16 13:26:15 -0600

    updated nav bar and removed orders from app.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,13 +9,10 @@ export default function NavBar({user, setUser}){
     }
     return (
         <nav>
-            <Link to="/orders">Order History</Link>
-            &nbsp; | &nbsp;
-            <Link to="/orders/new">New Order</Link>
-            &nbsp; | &nbsp;
             <Link to="/notes">Notes</Link>
-            <p>Welcome, {user.name}</p>
+            &nbsp; | &nbsp;
             <Link to="" onClick={ handleLogOut }>Log Out</Link>
+            <p>Welcome, {user.name}</p>
         </nav>
     );
 }
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,8 +1,6 @@
 import {useState} from "react";
 import { Routes, Route } from "react-router-dom";
 import AuthPage from "../AuthPage/AuthPage";
-import NewOrderPage from "../NewOrderPage/NewOrderPage";
-import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 import NotesPage from "../NotesPage/NotesPage";
 import NavBar from "../../components/NavBar/NavBar";
 import { getUser } from '../../utilities/users-service';
@@ -18,8 +16,6 @@ export default function App() {
       <NavBar user={user} setUser={setUser}/>
       <Routes>
         <Route path="/notes" element={<NotesPage notesArr={notesArr} setNotesArr={setNotesArr}/>} />
-        {/* <Route path="/orders" element={<OrderHistoryPage />} />
-        <Route path="/orders/new" element={<NewOrderPage />} /> */}
       </Routes>
       </>
       :
